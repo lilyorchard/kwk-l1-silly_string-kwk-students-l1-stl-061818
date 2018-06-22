e067ad5fd9e48ef4c683ecd8d2ce1512ac6b050f
lilyorchard <lily@orchardfamily.com>
2018-06-22 17:48:39 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -32,7 +32,7 @@ puts "There are #{letter_count} in #{big_word}"
 bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
 
 new_letter_count = bigger_word.size
-
+puts big_word.size
 puts "There are now #{new_letter_count} in #{bigger_word}"
 
 # Challenge 6: Capitalize this sentence properly
