4e5ff22e18f31c8affcf7b7a96b7d073f37fe087
lilyorchard <lily@orchardfamily.com>
2018-06-22 17:47:00 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -13,7 +13,7 @@ puts "MY FAVORITE WORD IS FAVORITE"
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
-phrase = "IM NOT SHOUTING"
+phrase = "IM NOT SHOUTING".downcase
 
 lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
