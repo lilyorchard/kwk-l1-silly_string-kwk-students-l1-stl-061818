1e6076851b095d192c542031e6e774d0a33a169a
lilyorchard <lily@orchardfamily.com>
2018-06-22 17:45:29 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,9 +1,9 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = "favorite"
+your_favorite_word = "favorite".upcase
 
-puts "My favorite word is favorite".capitalize
+puts "My favorite word is #{your_favorite_word}"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
